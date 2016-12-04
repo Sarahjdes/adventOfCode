2a38077d947ea6737077f8495be8d09785f8aa5d
sarahjdes <sjdesrochers@challengeu.com>
2016-12-03 22:33:51 -0500

    Day 03 Part 02 achieved, but needs some polishing

--- 2016/day03/day03.py
+++ 2016/day03/day03.py
@@ -7,10 +7,10 @@ shapes = []
 
 for i in t:
     numbers = map(int, i.split())
-    print numbers.sort()
+    numbers.sort()
     shapes.append(numbers)
 
-print shapes
+#print shapes
 
 possible_triangle = 0
 
@@ -22,4 +22,40 @@ for i in range(len(shapes)):
         #print h,' is bigger than ',c1+c2
         possible_triangle += 1
 
-print possible_triangle
+print 'Possible row triangle : ',possible_triangle
+
+## Part 02 ##
+
+#print t
+
+vertical = []
+vertical1 = []
+vertical2 = []
+vertical3 = []
+
+for i in t:
+    numbers = map(int, i.split())
+    #print numbers
+    vertical1.append(numbers[0])
+    vertical2.append(numbers[1])
+    vertical3.append(numbers[2])
+
+vertical = vertical1 + vertical2 + vertical3
+
+vertical_shapes = [vertical[i:i+3] for i in range(0, len(vertical), 3)]
+
+for i in vertical_shapes:
+    i.sort()
+
+#print vertical_shapes
+
+possible_vertical_triangle = 0
+
+for i in range(len(vertical_shapes)):
+    c1 = vertical_shapes[i][0]
+    c2 = vertical_shapes[i][1]
+    h = vertical_shapes[i][2]
+    if c1 + c2 > h:
+        possible_vertical_triangle += 1
+
+print 'Possible vertical triangle : ',possible_vertical_triangle
