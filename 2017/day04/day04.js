93d5cb553c02ad8565e276945dfa5a5747443179
sarahjdes <sjdesrochers@challengeu.com>
2017-12-04 12:32:28 -0500

    AOC day04 star2

--- 2017/day04/day04.js
+++ 2017/day04/day04.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
-    let arr = parser(data)
+    let arr = parser(data, false)
 
     let validCount = 0
+    let enforcedValidCount = 0
 
     for (i = 0; i < arr.length; i++) {
         console.log(arr[i])
@@ -24,14 +25,53 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
         }
     }
 
+    let arrEnforced = parser(data, true)
+
+    for (i = 0; i < arrEnforced.length; i++) {
+        console.log(arrEnforced[i])
+        let isUnique = true
+        arrEnforced[i].sort()
+        console.log(arrEnforced[i])
+        while (arrEnforced[i].length > 0 && isUnique == true) {
+            let current = arrEnforced[i][0]
+            arrEnforced[i].shift()
+            console.log('Looking for', current, 'in', arrEnforced[i])
+            if (arrEnforced[i].indexOf(current) == -1) {
+                console.log(current, 'is unique!')
+            } else {
+                console.log(current, 'is not unique')
+                isUnique = false
+            }
+        }
+        if (isUnique == true) {
+            enforcedValidCount ++
+        }
+    }
+
     console.log('Valid passphrases:', validCount)
+    console.log('Valid passphrases (enforced):', enforcedValidCount)
 })
 
-let parser = (data) => {
+let parser = (data, sort) => {
     let array = []
-    let rows = data.split('\n')
-    for (i = 0; i < rows.length; i++) {
-        array.push(rows[i].split(' '))
+    if (sort == false) {
+        let rows = data.split('\n')
+        for (i = 0; i < rows.length; i++) {
+            array.push(rows[i].split(' '))
+        }
+    } else if (sort == true) {
+        let rows = data.split('\n')
+        for (i = 0; i < rows.length; i++) {
+            let subArray = []
+            let words = rows[i].split(' ')
+            for (j = 0; j < words.length; j++) {
+                let letters = words[j].split('')
+                letters.sort()
+                let orderedLetters = letters.join('')
+                subArray.push(orderedLetters)
+            }
+            array.push(subArray)
+        }
     }
     return array
 }
