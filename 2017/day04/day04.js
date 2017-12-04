0285dbf5e0ab187d82005fc7d17f75d6b8062936
sarahjdes <sjdesrochers@challengeu.com>
2017-12-04 12:39:38 -0500

    AOC day04 clean

--- 2017/day04/day04.js
+++ 2017/day04/day04.js
@@ -1,55 +1,8 @@
 const fs = require('fs')
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
-    let arr = parser(data, false)
-
-    let validCount = 0
-    let enforcedValidCount = 0
-
-    for (i = 0; i < arr.length; i++) {
-        console.log(arr[i])
-        let isUnique = true
-        while (arr[i].length > 0 && isUnique == true) {
-            let current = arr[i][0]
-            arr[i].shift()
-            console.log('Looking for', current, 'in', arr[i])
-            if (arr[i].indexOf(current) == -1) {
-                console.log(current, 'is unique!')
-            } else {
-                console.log(current, 'is not unique')
-                isUnique = false
-            }
-        }
-        if (isUnique == true) {
-            validCount ++
-        }
-    }
-
-    let arrEnforced = parser(data, true)
-
-    for (i = 0; i < arrEnforced.length; i++) {
-        console.log(arrEnforced[i])
-        let isUnique = true
-        arrEnforced[i].sort()
-        console.log(arrEnforced[i])
-        while (arrEnforced[i].length > 0 && isUnique == true) {
-            let current = arrEnforced[i][0]
-            arrEnforced[i].shift()
-            console.log('Looking for', current, 'in', arrEnforced[i])
-            if (arrEnforced[i].indexOf(current) == -1) {
-                console.log(current, 'is unique!')
-            } else {
-                console.log(current, 'is not unique')
-                isUnique = false
-            }
-        }
-        if (isUnique == true) {
-            enforcedValidCount ++
-        }
-    }
-
-    console.log('Valid passphrases:', validCount)
-    console.log('Valid passphrases (enforced):', enforcedValidCount)
+    console.log('Valid passphrases:', validator(data, false))
+    console.log('Valid passphrases (enforced):', validator(data, true))
 })
 
 let parser = (data, sort) => {
@@ -74,4 +27,30 @@ let parser = (data, sort) => {
         }
     }
     return array
+}
+
+let validator = (data, enforced) =>  {
+    let array = parser(data, enforced)
+
+    let validCount = 0
+
+    for (i = 0; i < array.length; i++) {
+        let isUnique = true
+        while (array[i].length > 0 && isUnique == true) {
+            let current = array[i][0]
+            array[i].shift()
+            //console.log('Looking for', current, 'in', array[i])
+            if (array[i].indexOf(current) == -1) {
+                //console.log(current, 'is unique!')
+            } else {
+                //console.log(current, 'is not unique')
+                isUnique = false
+            }
+        }
+        if (isUnique == true) {
+            validCount ++
+        }
+    }
+    
+    return validCount    
 }
