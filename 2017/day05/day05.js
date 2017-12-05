1a4c9647f14f365c788847b965b2d2dfd080614e
sarahjdes <sjdesrochers@challengeu.com>
2017-12-05 09:20:40 -0500

    AOC day05 star2

--- 2017/day05/day05.js
+++ 2017/day05/day05.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
-    let maze = parser(data)
+    regularProcess(parser(data))
+    modifiedProcess(parser(data))
+})
+
+let parser = (input) => {
+    let array = input.split('\n')
+    return array.map(x => parseInt(x))
+}
+
+let regularProcess = (maze) => {
     let current = 0
     let steps = 0
 
@@ -12,10 +21,23 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
         steps++
     }
 
-    console.log('Steps needed:', steps)
-})
+    return console.log('Steps needed for regular process:', steps)
+}
 
-let parser = (input) => {
-    let array = input.split('\n')
-    return array.map(x => parseInt(x))
+let modifiedProcess = (maze) => {
+    let current = 0
+    let steps = 0
+
+    while(current < maze.length) {
+        let index = current
+        current += maze[index]
+        if (maze[index] >= 3) {
+            maze[index]--
+        } else {
+            maze[index]++
+        }
+        steps++
+    }
+
+    return console.log('Steps needed for modified process:', steps)
 }
