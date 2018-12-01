df4e0c1b800ea6deb6f1cac636c41762f770413a
Sarah-Jeanne Desrochers <sjdesrochers@challengeu.com>
2018-12-01 12:14:36 -0500

    day01 star 2

--- 2018/day01/day01.js
+++ 2018/day01/day01.js
@@ -6,4 +6,28 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
     const reducer = (acc, curr) => Number(acc) + Number(curr)
 
     console.log('resulting frequency:', arr.reduce(reducer))
+
+    let frequencies = []
+    let currentFrequency = 0
+    let twice
+
+    let loopIt = (arr) => {
+        frequencies.push(currentFrequency)
+        for (let i of arr) {
+            currentFrequency += Number(i)
+
+            if (frequencies.indexOf(currentFrequency) < 0){
+                frequencies.push(currentFrequency)
+            } else {
+                twice = currentFrequency
+                return
+            }
+        }
+        if (!twice)
+            loopIt(arr)
+    }
+
+    loopIt(arr)
+
+    console.log('first frequency to appear twice:', currentFrequency)
 })
