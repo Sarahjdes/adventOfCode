f701d6924e15a9eaf63c6b5cdae16e256ad9cb0f
Sarah-Jeanne Desrochers <sjdesrochers@challengeu.com>
2018-12-04 00:39:45 -0500

    day03 star 2

--- 2018/day03/day03.js
+++ 2018/day03/day03.js
@@ -2,8 +2,11 @@ const fs = require('fs')
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
     let arr = data.split('\n')
-    let fabric = []
+    let fabric = {}
     let overlapping = {}
+    let overlappingClaimsId = []
+
+    let allClaims = []
 
     for (let claim of arr) {
         let re = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/g
@@ -16,18 +19,38 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
             tall: Number(match[5])
         }
 
+        allClaims.push(claimData.id)
+
         for (let h = claimData.x; h < (claimData.wide + claimData.x); h++) {
             for (let v = claimData.y; v < (claimData.tall + claimData.y); v++) {
                 let square = h + ',' + v
 
-                if (fabric.indexOf(square) > -1)
+                if (fabric[square]) {
                     overlapping[square] = true
-                else 
-                    fabric.push(square)
+                    if (overlappingClaimsId.indexOf(claimData.id) == -1) {
+                        overlappingClaimsId.push(claimData.id)
+                    }
+                    if (overlappingClaimsId.indexOf(fabric[square]) == -1) {
+                        overlappingClaimsId.push(fabric[square])
+                    }
+                } else {
+                    fabric[square] = claimData.id
+                }
+
+                fabric[square] = claimData.id
             }
         }
     }
     let overlappingCount = Object.keys(overlapping).length
 
     console.log('overlapping squares:', overlappingCount)
+
+    let nonOverlapping
+
+    for (let id of allClaims) {
+        if (overlappingClaimsId.indexOf(id) == -1)
+            nonOverlapping = id
+    }
+
+    console.log('non overlapping claim:', nonOverlapping)
 })
